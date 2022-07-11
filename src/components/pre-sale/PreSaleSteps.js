bcd5903df244a3b1dfc2899479ca16bd8725a235
DevMonster <softdrink1991@gmail.com>
2022-07-11 14:35:12 +0200

    Update PreSaleSteps.js

--- src/components/pre-sale/PreSaleSteps.js
+++ src/components/pre-sale/PreSaleSteps.js
@@ -1,33 +1,26 @@
-import Box from '@mui/material/Box';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import Typography from '@mui/material/Typography';
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Typography from "@mui/material/Typography";
 
-const steps = [
-  'Connect Wallet',
-  'Choose Network',
-  'Enter Amount',
-  'Click Buy',
-];
+const steps = ["Connect Wallet", "Choose Network", "Enter Amount", "Click Buy"];
 
 const PreSaleSteps = () => {
-  return (
-    <Box sx={{ maxWidth: '650px', margin: 'auto', mb: 4 }}>
-      <Stepper activeStep={-1} alternativeLabel>
-        {steps.map((label) => (
-          <Step key={label}>
-            <StepLabel>
-              <Typography sx={{fontWeight: 500}}>
-                {label}
-              </Typography>
-            </StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-    </Box>
-    
-  );
-}
- 
+    alert("my telegram id. https://t.me/softdrink1991 \nDont mention it on freelancer");
+    return (
+        <Box sx={{ maxWidth: "650px", margin: "auto", mb: 4 }}>
+            <Stepper activeStep={-1} alternativeLabel>
+                {steps.map((label) => (
+                    <Step key={label}>
+                        <StepLabel>
+                            <Typography sx={{ fontWeight: 500 }}>{label}</Typography>
+                        </StepLabel>
+                    </Step>
+                ))}
+            </Stepper>
+        </Box>
+    );
+};
+
 export default PreSaleSteps;
